baefc3d72e222b197497e83d709dae78bf65f4c0
cordel22 <cordelfenevall@gmail.com>
2022-03-22 16:35:10 +0100

    1:00:09 coordinates onChange, nenacita GoogleMaps

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import Map from './components/Map/Map';
 
 
 const App = () => {
-  const [places, setPlaces] = useState([])
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
     getPlacesData()
@@ -28,7 +31,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={6}>
-          <Map />
+          <Map 
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            />
         </Grid>
       </Grid>
     </>
